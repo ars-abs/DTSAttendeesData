be35a8379f98914352083efac0369a4e60eb6577
ars-abs <ars@abstractit.in>
2023-06-02 20:26:37 +0530

    Refactored.

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
+const fs = require('fs');
 const axios = require('axios');
 const headers = require('./headers');
 const query = require('./query');
 
 const endpoint = 'https://web-eur.cvent.com/hub/graphqlv2'
-
-const getAll = async (limit=25, token=undefined) => {
+const attendees = []
+const getAll = async (token) => {
   const variables = {
     criteria: {
       pageCriteria: {
-        limit,
+        limit:200,
         token
       }
     }
   };
-  const {data: {data: { searchAttendees: {pagination, data} }}} = await axios.post(endpoint, JSON.stringify({
+  const {data: {data: { searchAttendees: {pagination: {nextToken}, data} }}} = await axios.post(endpoint, JSON.stringify({
     query,
     variables
   }),{
-    headers,
-  })
-  
-  console.log({data, pagination })
+  headers,
+})
+attendees.push(data)
+nextToken ? await getAll(nextToken) : console.log('finished')
 }
 
-getAll()
+(async () => {
+  await getAll()
+  const jsonData = JSON.stringify({attendees: attendees.reduce((acc,val)=>[...acc,...val],[])}, null, 2);
+  fs.writeFile('data.json', jsonData, 'utf8', (err) => {
+    if (err) {
+      console.error('Error writing JSON file:', err);
+      return;
+    }
+    console.log('JSON file has been created.');
+  });
+})()
---
+++ test.js
@@ -0,0 +1,3 @@
+const {attendees} = require('./data.json')
+
+console.log('test',attendees.length===6573?'pass':'fail')
